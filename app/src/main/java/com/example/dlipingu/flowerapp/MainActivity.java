d38ad145ff72705d7c133d8ae7cad7e80a6f41b3
deatram <ditram@outlook.com>
2016-10-24 23:04:19 +0300

    initial commit

---
+++ app/src/main/java/com/example/dlipingu/flowerapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.dlipingu.flowerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast msg = Toast.makeText(this, "Welcome to our app", Toast.LENGTH_LONG);
+        msg.show();
+    }
+}
