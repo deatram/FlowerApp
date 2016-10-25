cdf21e49b42205abeeeaa10f6b7b8ad0743fa645
deatram <ditram@outlook.com>
2016-10-25 16:25:52 +0300

    BackButton for the action bar

--- app/src/main/java/com/example/dlipingu/flowerapp/DetailActivity.java
+++ app/src/main/java/com/example/dlipingu/flowerapp/DetailActivity.java
@@ -9,5 +9,7 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 }
